886cf5d887407335eeeebfe1976736a36528f80f
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-11-09 19:21:31 +0100

    Improve dev toolkit

--- cmd/dev.go
+++ cmd/dev.go
@@ -2,11 +2,36 @@
 
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"path"
+	"runtime"
+	"time"
 
+	"github.com/joho/godotenv"
+)
+
+// Load environment configuration in development environment.
 func init() {
-	// Load environment configuration in development environment.
-	// Only works when running the program from the project root.
-	_ = godotenv.Load(".env")
-	_ = godotenv.Load(".env.dist")
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("cannot load environment: no caller information")
+	}
+
+	root := path.Clean(path.Join(path.Dir(filename), "../"))
+
+	_ = godotenv.Load(path.Join(root, ".env"))
+	_ = godotenv.Load(path.Join(root, ".env.dist"))
+
+	// Load defaults for info variables
+	if Version == "" {
+		Version = "<unknown>"
+	}
+
+	if CommitHash == "" {
+		CommitHash = "<unknown>"
+	}
+
+	if BuildDate == "" {
+		BuildDate = time.Now().Format(time.RFC3339)
+	}
 }
