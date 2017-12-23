75e58323d4c0856db88bff6a37e8e9cea23be9cb
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-12-23 23:33:22 +0100

    Fix cs

--- cmd/app.go
+++ cmd/app.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"github.com/deshboard/boilerplate-service/app"
-)
+import "github.com/deshboard/boilerplate-service/app"
 
 const FriendlyServiceName = app.FriendlyServiceName
 
